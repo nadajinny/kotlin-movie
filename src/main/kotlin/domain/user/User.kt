6783d225be0ec6fe442d1715c84ddaf63bf2a680
nadajinny <jinsun23007@gmail.com>
2026-04-13 09:49:34 +0900

    refactor: Point 내부 값을 기본적으로 주도로 설정

--- src/main/kotlin/domain/user/User.kt
+++ src/main/kotlin/domain/user/User.kt
@@ -3,9 +3,7 @@ package domain.user
 import domain.Id
 import util.ErrorMessage
 
-class User(val id: Id) {
-    var point = Point(2000)
-
+class User(val id: Id, var point: Point = Point(2000)) {
     init {
         require(id.value > 0) { ErrorMessage.ID_MUST_BE_POSITIVE }
     }
