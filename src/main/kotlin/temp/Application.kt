a281aaf1efeaf65ba141566db7721228403a2b93
nadajinny <jinsun23007@gmail.com>
2026-04-13 11:03:01 +0900

    feat: name 인자를 포함한 인사말 전달

--- src/main/kotlin/temp/Application.kt
+++ src/main/kotlin/temp/Application.kt
@@ -2,6 +2,24 @@ package temp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class Greeting(
+    val id: Long,
+    val name: String
+) {
+    fun sayHello() : String = "Hello, $name!"
+}
+
+@RestController
+class GreetingController {
+    @GetMapping("/greeting")
+    fun hello(): Greeting {
+        return Greeting(1L, "Ember")
+    }
+}
+
 
 @SpringBootApplication
 class Application
